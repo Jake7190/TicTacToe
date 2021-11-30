7e4af2f9b48db168fc000a664a299e0280c98bbc
Jacob McMahon <93213018+Jake7190@users.noreply.github.com>
2021-11-29 19:48:25 -0500

    v1.0.0

---
+++ Game.cs
@@ -0,0 +1,154 @@
+﻿//Project name: Module 4 Exercise 1
+//Project purpose: Handle Game Funcitonality
+//Created by: Jacob McMahon
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TicTacToe
+{
+
+    //.Net does not have a BadMoveException so you must define a custom exception class
+    class BadMoveException : Exception
+    {
+        public BadMoveException()
+        {
+        }
+
+        public BadMoveException(string message)
+            : base(message)
+        {
+        }
+
+        public BadMoveException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    internal class Game
+    {
+        char[,] grid = new char[3, 3];
+        public Game()
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    grid[x, y] = '.';
+                }
+            }
+        }
+
+
+        public void Print()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Key");
+            int cell = 0;
+            for (int y = 0; y < 3; y++)
+            {
+                if (y >= 1) Console.WriteLine("---+---+---");
+                for (int x = 0; x < 3; x++)
+                {
+                    if (x >= 1) Console.Write("|");
+                    Console.Write($" {cell++} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int y = 0; y < 3; y++)
+            {
+                if (y >= 1) Console.WriteLine("---+---+---");
+                for (int x = 0; x < 3; x++)
+                {
+                    if (x >= 1) Console.Write("|");
+                    Console.Write($" {grid[x, y]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        internal void Move(int position, char player)
+        {
+            //TBD: This function needs validation - Done
+            //1. Position needs to be in the range 0..9 - if not throw an ArgumentException
+            //2. The grid cell needs to be unoccupied - if not throw a BadMoveException
+            //    .Net does not have a BadMoveException so you must define a custom exception class
+
+
+            if (position < 0 || position > 9)
+            {
+                throw new ArgumentException("Postion out of range");
+            }
+            if (grid[position % 3, (position / 3)] != '.')
+            {
+                throw new BadMoveException("Error:Bad Move!!");
+            }
+
+            grid[position % 3, (position / 3)] = player;
+        }
+
+        public bool Ended() //Check if game should end
+        {
+            //TBD: Determine the 'end' condition - Done
+            bool result = false;
+            bool allDotsCovered = true;
+            int all_o = 'O' + 'O' + 'O';
+            int all_x = 'X' + 'X' + 'X';
+
+            for (int i = 0; i < 3; i++)
+            {
+                int coloumn_total = 0;
+                int row_total = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (grid[i, j] == '.')
+                    {
+                        allDotsCovered = false;
+                    }
+
+                    coloumn_total += grid[i, j];
+                    row_total += grid[j, i];
+
+                }
+
+                if (coloumn_total == all_o || coloumn_total == all_x)
+                {
+                    result = true;
+                    break;
+                }
+
+                if (row_total == all_o || row_total == all_x)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            int right_diag = grid[2, 0] + grid[1, 1] + grid[0, 2];
+            int left_diag = grid[0, 0] + grid[1, 1] + grid[2, 2];
+
+
+            if (left_diag == all_o || left_diag == all_x)
+            {
+                result = true;
+            }
+
+            if (right_diag == all_o || right_diag == all_x)
+            {
+                result = true;
+            }
+
+            if (allDotsCovered)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//Project name: Module 4 Exercise 1
+//Project purpose: Handle game start and exit   
+//Created by: Jacob McMahon
+
+using System;
+
+namespace TicTacToe
+{
+    class Program
+
+    {
+        static void Main()
+        {
+            var game = new Game();
+            var player = 'X';
+            while (!game.Ended())
+            {
+                game.Print();
+                //Get an integer position
+                Console.Write("Enter your move:");
+                string input;
+                int position;
+                do
+                {
+                    input = Console.ReadLine();
+
+                } while (!int.TryParse(input, out position));
+
+                //TBD: Handle the Exceptions:
+                //1. Once you have implemented exceptions in game.Move() you must catch the ones you expect to see here
+                //and handle them with suitable error messages
+                try
+                {
+                    game.Move(position, player);
+                }
+                catch (ArgumentException err)
+                {
+                    System.Diagnostics.Debug.Write(err.Message);
+                    // This is wrong.
+                    throw err;
+                }
+                catch (Exception err) //2. Also handle any unexpected exceptions with an "Unknown Error" message. Work out a way to test that code!
+                {
+                    System.Diagnostics.Debug.Write("Unknown Error");
+                    throw err;
+                }
+
+                //Switch players
+                player = (player == 'X') ? 'O' : 'X';
+            }
+            game.Print();
+        }
+    }
+}
